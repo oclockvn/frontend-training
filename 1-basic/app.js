8ea54857cb16bd7cd49157d4eab2ed59aeeecdab
Quang Phan <79438276+quangphantally@users.noreply.github.com>
2023-08-08 14:17:44 +0700

    add title case function

--- 1-basic/app.js
+++ 1-basic/app.js
@@ -6,4 +6,30 @@ function sayHello() {
   result.innerText = `Hello, ${value}`;
 }
 
+// pure function
+function toTitleCase(input) {
+  if (!input) {
+    return '';
+  }
+
+  const len = input.length;
+  let result = input[0].toUpperCase();
+
+  for (let i = 1; i < len; i++) {
+    if (input[i - 1] === ' ') {
+      result += input[i].toUpperCase();
+    } else {
+      result += input[i].toLowerCase();
+    }
+  }
+
+  return result;
+}
+
 document.getElementById('btn-say-hello').addEventListener('click', sayHello);
+document.getElementById('btn-title-case').addEventListener('click', function () {
+  const input = document.getElementById('input');
+  const result = document.getElementById('result1');
+
+  result.innerText = toTitleCase(input.value);
+});
